a0bb418d8df0fbe5e14dda3b83caac0e6bb96cbf
Venkatesh <lankevenkatesh314@gmail.com>
2026-08-23 20:22:35 +0530

    Fix User entity column mappings for snake_case database columns
    - Add @Column annotations to map camelCase fields to snake_case columns
    - firstName -> first_name
    - lastName -> last_name
    - mobileNumber -> mobile_number
    - createdAt -> created_at
    - updatedAt -> updated_at
    - Resolves SQL grammar error in user registration

--- UserManagement/UserService/src/main/java/com/ashok/it/userservice/Entity/User.java
+++ UserManagement/UserService/src/main/java/com/ashok/it/userservice/Entity/User.java
@@ -1,6 +1,7 @@
 package com.ashok.it.userservice.Entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,19 +18,24 @@ public class User {
     @Id
     private Long id;
 
+    @Column("first_name")
     private String firstName;
 
+    @Column("last_name")
     private String lastName;
 
     private String email;
 
+    @Column("mobile_number")
     private String mobileNumber;
 
     private String password;
 
     private String status;
 
+    @Column("created_at")
     private LocalDateTime createdAt;
 
+    @Column("updated_at")
     private LocalDateTime updatedAt;
 }
