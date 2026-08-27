79b05121c69589389e17a43066aa8c6e32d4779d
Lanke Venkatesh <139674961+LankeVenkatesh-Java-Developer@users.noreply.github.com>
2026-08-27 17:15:19 +0530

    Delete UserManagement/UserService directory

--- UserManagement/UserService/src/main/java/com/ashok/it/userservice/Exception/AddressNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ashok.it.userservice.Exception;
-
-public class AddressNotFoundException extends RuntimeException{
-    public AddressNotFoundException(String message) {
-        super(message);
-    }
-}
--- UserManagement/UserService/src/main/java/com/ashok/it/userservice/Exception/DuplicateUserException.java
+++
@@ -1,7 +0,0 @@
-package com.ashok.it.userservice.Exception;
-
-public class DuplicateUserException  extends RuntimeException{
-    public DuplicateUserException(String message) {
-        super(message);
-    }
-}
